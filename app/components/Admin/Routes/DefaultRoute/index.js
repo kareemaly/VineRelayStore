0af304fd3fdec53e9293e380ab12edd6feeb908f
Kareem Mohamed <kareem3d.a@gmail.com>
2017-07-04 09:02:31 +0200

    Refactor Default Route to have both the query and route in the same file

--- app/components/Admin/Routes/DefaultRoute/index.js
+++ app/components/Admin/Routes/DefaultRoute/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { QueryRenderer, graphql } from 'react-relay';
+import { createFragmentContainer, QueryRenderer, graphql } from 'react-relay';
+import { withRouter } from 'react-router';
 import relayEnvironment from 'app/config/relay';
 import PageError from 'app/components/Common/PageError';
 import PageLoader from 'app/components/Common/PageLoader';
-import DefaultRoute from './DefaultRoute';
+import DashboardLayout from 'app/components/Admin/Main/DashboardLayout';
+
+class DefaultRoute extends React.Component {
+  componentWillMount() {
+    // Not an admin so we need to change route
+    if(! this.props.viewer.isAdmin) {
+      this.props.history.replace(`/admin/login`);
+    }
+  }
+
+  render() {
+    const {
+      viewer,
+    } = this.props;
+
+    return (
+      <DashboardLayout viewer={viewer}>
+        <div>Welcome to dashboard {viewer.firstName}!</div>
+      </DashboardLayout>
+    );
+  }
+}
+
+const DefaultRouteContainer = createFragmentContainer(
+  withRouter(DefaultRoute),
+  graphql`
+    fragment DefaultRoute_viewer on User {
+      firstName
+      isAdmin
+      ...DashboardLayout_viewer
+    }
+  `
+);
 
 export default () => (
   <QueryRenderer
@@ -21,7 +54,7 @@ export default () => (
       }
 
       if (props) {
-        return <DefaultRoute {...props} />;
+        return <DefaultRouteContainer {...props} />;
       }
 
       return <PageLoader />;
