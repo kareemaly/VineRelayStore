96f1f5c0f0485f9e21ffd3de26e6e23937643de0
Kareem Mohamed <kareem3d.a@gmail.com>
2017-07-10 21:59:12 +0200

    Add routes to store router

--- app/routes/StoreRouter.js
+++ app/routes/StoreRouter.js
@@ -3,6 +3,11 @@ import { ThemeProvider } from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import defaultTheme from 'app/themes/store/default';
 
+import BrandsRoute from 'app/components/Store/Routes/BrandsRoute';
+import BrandRoute from 'app/components/Store/Routes/BrandRoute';
+import CategoriesRoute from 'app/components/Store/Routes/CategoriesRoute';
+import CategoryRoute from 'app/components/Store/Routes/CategoryRoute';
+import ProductRoute from 'app/components/Store/Routes/ProductRoute';
 import HomeRoute from 'app/components/Store/Routes/HomeRoute';
 
 export default () => (
@@ -14,6 +19,38 @@ export default () => (
         path='/'
         component={HomeRoute}
       />
+      {/* List brands Route */}
+      <Route
+        path='/brands'
+        component={BrandsRoute}
+      />
+      {/* Show brand Route */}
+      <Route
+        exact
+        path='/brand/:brandId'
+        component={BrandRoute}
+      />
+
+      {/* List categories Route */}
+      <Route
+        path='/categories'
+        component={CategoriesRoute}
+      />
+      {/* Show category Route */}
+      <Route
+        exact
+        path='/category/:categoryId'
+        component={CategoryRoute}
+      />
+
+      {/* Show product Route */}
+      <Route
+        exact
+        path='/product/:productId'
+        component={ProductRoute}
+      />
+
+
       <Route
         exact
         path='/products'
